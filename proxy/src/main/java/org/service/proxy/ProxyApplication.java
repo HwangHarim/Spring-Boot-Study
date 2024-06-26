b782fec49251785cf74d4393eead66c008f8f0c1
HaRim <76032947+HwangHarim@users.noreply.github.com>
2024-06-26 12:19:14 +0900

    feature : proxy, Decorator 패턴 스터디

---
+++ proxy/src/main/java/org/service/proxy/ProxyApplication.java
@@ -0,0 +1,26 @@
+package org.service.proxy;
+
+
+import org.service.proxy.config.v1_proxy.InterfaceProxyConfig;
+import org.service.proxy.trace.logtrace.LogTrace;
+import org.service.proxy.trace.logtrace.ThreadLocalLogTrace;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+//@Import({AppV1Config.class, AppV2Config.class})
+@Import(InterfaceProxyConfig.class)
+@SpringBootApplication(scanBasePackages = "org.service.proxy.app.v3")
+public class ProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyApplication.class, args);
+    }
+
+
+    @Bean
+    public LogTrace logTrace(){
+        return new ThreadLocalLogTrace();
+    }
+}
