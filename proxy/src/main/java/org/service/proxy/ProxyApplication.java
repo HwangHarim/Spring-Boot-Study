fae1f4a76eb5e0b3440fc4c3718baecc1563454e
HaRim <76032947+HwangHarim@users.noreply.github.com>
2024-07-03 11:44:04 +0900

    feature : spring AOP study

--- proxy/src/main/java/org/service/proxy/ProxyApplication.java
+++ proxy/src/main/java/org/service/proxy/ProxyApplication.java
@@ -9,6 +9,7 @@ import org.service.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import org.service.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import org.service.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
 import org.service.proxy.config.v5_autoproxy.AutoProxyConfig;
+import org.service.proxy.config.v6_aop.AopConfig;
 import org.service.proxy.trace.logtrace.LogTrace;
 import org.service.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -24,7 +25,8 @@ import org.springframework.context.annotation.Import;
 //@Import(ProxyFactoryConfigV1.class)
 //@Import(ProxyFactoryConfigV2.class)
 //@Import(BeanPostProcessorConfig.class)
-@Import(AutoProxyConfig.class)
+//@Import(AutoProxyConfig.class)
+@Import(AopConfig.class)
 @SpringBootApplication(scanBasePackages = "org.service.proxy.app.v3")
 public class ProxyApplication {
 
