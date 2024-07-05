f57891cdd6867707e3733d7dcf6f686cb1a7ddd6
HaRim <76032947+HwangHarim@users.noreply.github.com>
2024-07-05 11:49:12 +0900

    feature : spring jdbc, data source study

---
+++ jdbc/src/main/java/com/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package com.jdbc.connection;
+
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:mysql://localhost:3306/jdbc_study_db?serverTimezone=Asia/Seoul&useSSL=false";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "";
+}
