bd0be6b121a23eb7483235bc322007cc2f644db6
HaRim <76032947+HwangHarim@users.noreply.github.com>
2024-07-15 10:55:02 +0900

    feature : spring JDBC2 예제 구현

---
+++ itemservice-db/src/main/java/com/itemservicedb/ItemserviceDbApplication.java
@@ -0,0 +1,25 @@
+package com.itemservicedb;
+
+import com.itemservicedb.config.MemoryConfig;
+import com.itemservicedb.repository.ItemRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+
+@Import(MemoryConfig.class)
+@SpringBootApplication(scanBasePackages = "com.itemservicedb.web")
+public class ItemserviceDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemserviceDbApplication.class, args);
+    }
+
+    @Bean
+    @Profile("local")
+    public TestDataInit testDataInit(ItemRepository itemRepository) {
+        return new TestDataInit(itemRepository);
+    }
+
+}
