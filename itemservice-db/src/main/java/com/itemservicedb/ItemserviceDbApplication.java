b09dc3253a525c7457819821e1affa2df9a00811
HaRim <76032947+HwangHarim@users.noreply.github.com>
2024-07-16 12:16:14 +0900

    feature : spring JdbcTemplate test study

--- itemservice-db/src/main/java/com/itemservicedb/ItemserviceDbApplication.java
+++ itemservice-db/src/main/java/com/itemservicedb/ItemserviceDbApplication.java
@@ -2,6 +2,7 @@ package com.itemservicedb;
 
 import com.itemservicedb.config.JdbcTemplateV2Config;
 import com.itemservicedb.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
+@Slf4j
 @Import(JdbcTemplateV2Config.class)
 @SpringBootApplication(scanBasePackages = "com.itemservicedb.web")
 public class ItemserviceDbApplication {
@@ -23,5 +25,17 @@ public class ItemserviceDbApplication {
     public TestDataInit testDataInit(ItemRepository itemRepository) {
         return new TestDataInit(itemRepository);
     }
+
+//    @Bean
+//    @Profile("test")
+//    public DataSource dataSource() {
+//        log.info("메모리 데이터베이스 초기화");
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("org.hw.Driver");
+//        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+//        dataSource.setUsername("sa");
+//        dataSource.setPassword("");
+//        return dataSource;
+//    }
 
 }
