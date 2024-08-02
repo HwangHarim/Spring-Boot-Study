11dc93f0312dcac8b57b9e5fb807837a703e74e6
HaRim <76032947+HwangHarim@users.noreply.github.com>
2024-08-02 11:18:36 +0900

    feature : Spring Transactional 전파 study

--- springtx/src/test/java/com/springtx/propagation/BasicTxTest.java
+++ springtx/src/test/java/com/springtx/propagation/BasicTxTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
@@ -126,5 +127,28 @@ public class BasicTxTest {
 
         log.info("외부 트랜잭션 커밋");
         Assertions.assertThatThrownBy(() -> txManager.commit(outer)).isInstanceOf(UnexpectedRollbackException.class);
+    }
+
+    @Test
+    void inner_rollback_requires_new() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer .isNewTransaction() ={}", outer.isNewTransaction());
+
+        inner();
+
+        log.info("외부 트랜잭션  커밋");
+        txManager.commit(outer);
+    }
+
+    private void inner() {
+        log.info("내부 트랜잭션 시작");
+        DefaultTransactionAttribute definition = new DefaultTransactionAttribute();
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);// 기존 트랜잭션 무시 새로운 트랜잭션 생성
+        TransactionStatus inner = txManager.getTransaction(definition);
+        log.info("inner .isNewTransaction() ={}", inner.isNewTransaction());//true
+
+        log.info("내부 트랜잭션 롤백");
+        txManager.rollback(inner);
     }
 }
